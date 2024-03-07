01bf73f517f1898ef44b78d9eeb051de2b4cac43
Sai Kiran <124028496+saikiran-19906@users.noreply.github.com>
2024-03-07 15:02:15 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Export.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Export_2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
